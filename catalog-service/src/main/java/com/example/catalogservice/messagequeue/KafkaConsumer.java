a7bc98fb50bec0f944f1f5eee165e1661262c213
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-04-05 00:49:17 +0900

    Catalogs Microservice 컨슈머 설정 수정

--- catalog-service/src/main/java/com/example/catalogservice/messagequeue/KafkaConsumer.java
+++ catalog-service/src/main/java/com/example/catalogservice/messagequeue/KafkaConsumer.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KafkaConsumer {
 
-    CatalogRepository repository;
+    private final CatalogRepository repository;
 
     @KafkaListener(topics = "example-order-topic")
     public void processMessage(String kafkaMessage) {
